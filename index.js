9fa27391fdf650614342f21664bbef6fa53769b2
Sergey Rura <sergey_rura@intuit.com>
2022-05-23 02:15:44 +0300

    QUick and dirty

--- index.js
+++ index.js
@@ -4,45 +4,64 @@ const process = require('process');
 const port = +process.argv[2] || 3000;
 const client = require('redis').createClient();
 
+const cards = fs.readJsonSync('./cards.json').map((card) =>
+    Buffer.from(`{"id": "${card.id}", "name": "${card.name}"}`, 'ascii')
+);
+
+const cardsDone = Buffer.from('{"id":"ALL CARDS"}', 'ascii');
+const users = {};
 let START;
 let END;
-const cards = fs.readJsonSync('./cards.json').map((card) =>
-    Buffer.from(`{"id": "${card.id}", "name": "${card.name}"}`)
-);
-const cardsDone = Buffer.from(`{"id": "ALL CARDS"}`);
-const users = {};
+
+const wait = async (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
 
 const prepareServer = async () => {
-    const cardsAssignmentCount = cards.length / 2;
-
     const servicesKey = 'SERVICES';
     const identifier = process.pid.toString();
     await client.RPUSH(servicesKey, identifier);
-    const servicesList = await client.LRANGE(servicesKey, 0, -1);
-    const index = servicesList.indexOf(identifier);
 
-    console.log(`Process #${process.pid} was assigned to index #${index}`);
-    if (index === 0) {
-        END = cards.length;
-        START = END - cardsAssignmentCount + 1;
-    } else {
-        END = cards.length - cardsAssignmentCount;
-        START = END - cardsAssignmentCount + 1;
-    }
+    await wait(500);
+    const cardsCount = cards.length;
+    const services = await client.LRANGE(servicesKey, 0, -1);
+    const index = services.indexOf(identifier);
+
+    const amount = Math.floor(cardsCount / services.length)
+    const remainder = (cardsCount) % services.length
+    const offset = remainder ? (remainder < index ? remainder : index) : 0
+    const hasRemainder = remainder > index
+
+    START = amount * index + offset
+    END = amount * (index + 1) + (hasRemainder ? 1 : 0) + offset
+    //
+    // console.log(`
+    //     Process #${process.pid}
+    //     Index: ${index}
+    //     Assignment: ${amount};
+    //     Cards count: ${cardsCount};
+    //     Remainder: ${remainder};
+    //     Remainder offset: ${offset};
+    //     Should add remainder: ${hasRemainder};
+    //     Start ${START};
+    //     End ${END}`
+    // );
 }
-
 const onRequest = async (req, res) => {
-    let user = users[req.url] ++;
-    if (user) {
-        const card = user < END ? cards[user] : cardsDone
-        res.setHeader('Content-Length', card.length)
-        res.write(card)
+    let user;
+    if (users[req.url] === undefined) {
+        user = users[req.url] = START;
     } else {
-        users[req.url] = START;
-        const card = cards[START - 1]
-        res.setHeader('Content-Length', card.length)
-        res.write(card)
+        user = ++users[req.url]
     }
+
+    if (user < END) {
+        const card = cards[user];
+        res.setHeader('Content-Length', card.length)
+        return res.write(card)
+    }
+    res.setHeader('Content-Length', cardsDone.length);
+    return res.write(cardsDone);
 }
 
 const onRedisReady = async () => {
